829c07b9d5d9284b66aa3a8318b02291ca5f69a1
Abubakar Sani Muhammad <abubakarsanim01@gmail.com>
2022-10-13 18:17:24 -0700

    6 testing 3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/** the heading and
+/**
+ * the heading and
  *  start if
  *   the main
  *   ok
@@ -12,6 +13,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
