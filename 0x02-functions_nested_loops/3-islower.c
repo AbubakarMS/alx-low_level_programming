f483c429a554309a3ebccffbbb16a2e371b77f6e
Abubakar Sani Muhammad <abubakarsanim01@gmail.com>
2022-10-19 15:02:20 -0700

    islower test 2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase 
+ * _islower - checks for lowercase
  *@c: the character to be checked
  * Return: Always 0
  */
@@ -10,9 +10,9 @@ int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else
-		return(0);
+		return (0);
 }
 
