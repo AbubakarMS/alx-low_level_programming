576fe90bef85aca070801fb5e2693121ef59ad2c
Abubakar Sani Muhammad <abubakarsanim01@gmail.com>
2022-10-19 14:59:21 -0700

    islower and main.h update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase 
+ *@c: the character to be checked
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return(1);
+	}
+	else
+		return(0);
+}
+
