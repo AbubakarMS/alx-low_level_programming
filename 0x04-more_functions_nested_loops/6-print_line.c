5b2f6d6228e4bcaecee30b7df012c0cdbaf30e6a
Abubakar Sani Muhammad <abubakarsanim01@gmail.com>
2022-10-20 05:54:09 -0700

    print line updte 1

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - ok
+ *@n: Char to be printed
+ * Return: ok
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
