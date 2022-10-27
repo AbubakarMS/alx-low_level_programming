50b017625909971453cb0389a2153099dc95d6b2
Abubakar Sani Muhammad <abubakarsanim01@gmail.com>
2022-10-27 15:12:04 -0700

    rot 13 update 4

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,17 +2,17 @@
 
 /**
  * rot13 - encode
- * @n: char
+ * @s: char
  * Return: n
  */
 
-char *rot13(char *n)
+char *rot13(char *s)
 {
 	int i, j;
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
 		{
