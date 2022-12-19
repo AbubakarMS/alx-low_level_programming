dada99923d7a4c4a36c98091ad29a5c88dfc4be1
Abubakar Sani Muhammad <abubakarsanim01@gmail.com>
2022-12-19 03:03:16 -0800

    Corrections on missing projects 5

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - ok
+ * @s: ok
+ * @b :ok
+ * @n : ok
+ * Return: ko
+ */
+
+har *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
